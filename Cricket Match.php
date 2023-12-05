8ae7c27eb9754f840f595b616b8108e1dfdf64d4
Md.Imran Ahmed <145373877+imranahmed234@users.noreply.github.com>
2023-12-05 21:15:46 +0600

    Add files via upload

---
+++ Cricket Match.php
@@ -0,0 +1,57 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            "**************Today Cricket Match*****05-Dec-23**************<br>
+            
+        
+        </head>
+        
+        
+        <body>
+
+            <Button align='center'style="background-color:Green;">B</Button>
+            <Button align='center'style="background-color:green;">N</Button>
+            <Button align='center'style="background-color:green;">C</Button>
+            <Button align='center'style="background-color:green;">T</Button>
+               <Button align='center'style="background-color:Red;">VS</Button>
+            <Button align='center'style="background-color:yellow;">Y</Button>
+            <Button align='center'style="background-color:yellow;">O</Button>
+            <Button align='center'style="background-color:yellow;">U</Button>
+            <Button align='center'style="background-color:yellow;">N</Button>
+            <Button align='center'style="background-color:yellow;">G</Button>
+            <Button align='center'style="background-color:yellow;">B</Button>
+            <Button align='center'style="background-color:yellow;">O</Button>
+            <Button align='center'style="background-color:yellow;">Y</Button>
+            <Button align='center'style="background-color:yellow;">S</Button></br>
+            <Button align='center'> TOSS</txt><Button>WIN</Button><Button>*YOUNG Boys*</Button><br>
+            <Button align='center'style="background-color:yellow;">YOUNG BOYES TARGATE:180</Button><br>
+            <Button align='center'style="background-color:Green;">B.N.C.T=143</Button>
+           
+
+
+
+         
+        </body>
+    
+    </html></br>
+   
+   
+   
+   
+   <?php
+
+echo"##### Today Match Details<br>";
+echo" B.N.C.T VS YOUNGBOYS<br>";
+echo" TOSS WIN= YOUNGBOYS<br>";
+echo" YOUNGBOYS TARGET=180<br>";
+$YOUNGBOYSTARGET=180.;
+echo" B.N.C.T=143";
+$BNCT=143;
+echo"<br>";
+echo"YOUNGBOYS WIN=";
+echo $YOUNGBOYSTARGET-$BNCT;
+
+echo"Runs";
+
+
+?>
